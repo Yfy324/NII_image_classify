d5bf613387b8b75ca60186ff0b11bceb2a927201
Yfy324 <y@kongda.space>
2021-12-07 11:39:30 +0800

    finish the task in NII SSP program

--- Multi_classification/4-class.py
+++ Multi_classification/4-class.py
@@ -1,23 +1,23 @@
-import dataset_split
-from img_fea_generator import *
-from sift_bof import *
+from Multi_classification import dataset_split
+from Multi_classification.img_fea_generator import *
+from Multi_classification.sift_bof import *
 
 if __name__ == '__main__':
-    # original = '/home/yfy/Desktop/projects/ImageNet/fly/'
-    # base = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/'
-    # dataset_split.dir_creat(original, base)
-    #
-    # dic_path = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/dic/'
-    # n_features = 150
-    # im_paths_dic, im_classes_dic = path_label(dic_path, 1)
-    # describe_dic, dic, d_list_dic = sift_descriptor_voc(im_paths_dic, n_features)
-    # im_features_dic, im_distribution = bof_features(im_paths_dic, dic, d_list_dic, n_features, 1)
-    # im_data = data_merge(im_classes_dic, im_paths_dic, im_features_dic)
-    # joblib.dump((im_distribution, n_features, dic), "4c_bof150_features.pkl", compress=3)
+    original = '/home/yfy/Desktop/projects/ImageNet/fly/'
+    base = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/'
+    dataset_split.dir_creat(original, base)
+
+    dic_path = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/dic/'
+    n_features = 150
+    im_paths_dic, im_classes_dic = path_label(dic_path, 1)
+    describe_dic, dic, d_list_dic = sift_descriptor_voc(im_paths_dic, n_features)
+    im_features_dic, im_distribution = bof_features(im_paths_dic, dic, d_list_dic, n_features, 1)
+    im_data = data_merge(im_classes_dic, im_paths_dic, im_features_dic)
+    joblib.dump((im_distribution, n_features, dic), "4c_features_last.pkl", compress=3)
 
     train_path = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/train'
-    im_distribution, n_features, dic = joblib.load('4c_bof150_features.pkl')
-    aug = 2
+    im_distribution, n_features, dic = joblib.load('4c_features_last.pkl')
+    aug = 3
     aug_size = 1 + aug
     im_paths_tr, im_classes_tr = path_label(train_path, aug_size)
     d_list_tr = aug_generate_descriptors(im_paths_tr, aug_size)
@@ -28,14 +28,14 @@ if __name__ == '__main__':
     im_features_tr = generate_features(im_paths_tr, aug_size, d_list_tr, dic, n_features, im_distribution)
     data_train = data_merge(im_classes_tr, im_paths_tr, im_features_tr)
     train1 = pd.DataFrame(data_train)
-    train1.to_csv('4c_2_train_150.csv')
+    train1.to_csv('4c_train_last.csv')
 
-    # val_path = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/validation/'
-    # im_distribution, n_features, dic = joblib.load('4c_bof150_features.pkl')
-    # b_size = 1
-    # im_paths_val, im_classes_val = path_label(val_path, b_size)
-    # d_list_val = generate_descriptors(im_paths_val)
-    # im_features_val = generate_features(im_paths_val, b_size, d_list_val, dic, n_features, im_distribution)
-    # data_val = data_merge(im_classes_val, im_paths_val, im_features_val)
-    # val1 = pd.DataFrame(data_val)
-    # val1.to_csv('4c_1_val_150.csv')
+    val_path = '/home/yfy/Desktop/projects/ImageNet/fly/fly_small/validation/'
+    im_distribution, n_features, dic = joblib.load('4c_bof150_features.pkl')
+    b_size = 1
+    im_paths_val, im_classes_val = path_label(val_path, b_size)
+    d_list_val = generate_descriptors(im_paths_val)
+    im_features_val = generate_features(im_paths_val, b_size, d_list_val, dic, n_features, im_distribution)
+    data_val = data_merge(im_classes_val, im_paths_val, im_features_val)
+    val1 = pd.DataFrame(data_val)
+    val1.to_csv('4c_val_last.csv')
